8916c91854fb474f76633f205200035dbe9a06c1
Rolf Piepenbrink <rolf.piep.mobiel@gmail.com>
2024-04-02 17:38:52 -0400

    Individual camera movement

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -18,10 +18,36 @@ public class CameraMovement : MonoBehaviour
     {
         // if (EventSystem.current.IsPointerOverGameObject()) return;
         if (Input.GetMouseButton(0)) return; // Left.
-        if (Input.GetMouseButton(1)) MouseRightClick(); // Right.
-        else if (Input.GetMouseButton(2)) MouseMiddleClick(); // Middle.
+        if (Input.GetMouseButton(1) && CameraMayMove()) MouseRightClick(); // Right.
+        else if (Input.GetMouseButton(2) && CameraMayMove()) MouseMiddleClick(); // Middle.
         else if (Input.GetMouseButtonUp(1) || Input.GetMouseButtonUp(2)) UnlockMouse();
         else if (Input.mouseScrollDelta.y != 0) ZoomCamera(Input.mouseScrollDelta.y);
+    }
+
+    private bool CameraMayMove()
+    {
+        return MouseInViewport() || _mouseLocked;
+    }
+
+    private bool MouseInViewport()
+    {
+        var (mX, mY) = (Input.mousePosition.x, Input.mousePosition.y);
+        var x = Camera.rect.x;
+        var y = Camera.rect.y;
+        var width = Camera.rect.width;
+        var height = Camera.rect.height;
+
+        var sWidth = Screen.width;
+        var sHeight = Screen.height;
+
+        if (!InRange(mX, x * sWidth, width * sWidth)) return false;
+        if (!InRange(mY, y * sHeight, height * sHeight)) return false;
+        return true;
+    }
+
+    private bool InRange(float value, float start, float length)
+    {
+        return value >= start && value <= start + length;
     }
 
     private void MouseMiddleClick()
