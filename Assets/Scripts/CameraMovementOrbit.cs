5f22ae26457eb56aa566b5fa79de8530137201a8
Rolf Piepenbrink <rolf.piep.mobiel@gmail.com>
2024-08-05 16:56:05 +0200

    Pan camera with left mouse button

--- Assets/Scripts/CameraMovementOrbit.cs
+++ Assets/Scripts/CameraMovementOrbit.cs
@@ -7,25 +7,41 @@ public class CameraMovementOrbit : MonoBehaviour
     private bool _mouseLocked;
 
     public Vector3 orbitOrigin;
+    private Vector3 _gridCenter;
     private float _distanceFromOrigin = 10;
     private bool _manualOrigin;
+    
 
     private void Update()
     {
         MouseInput();
+        if (!_gridCenter.Equals(XWFCAnimator.Instance.GetGridCenter()))
+        {
+            _gridCenter = XWFCAnimator.Instance.GetGridCenter();
+            orbitOrigin = _gridCenter;
+        }
         if (_manualOrigin) return;
-        var gridCenter = XWFCAnimator.Instance.GetGridCenter();
-        if (orbitOrigin.Equals(gridCenter)) return;
-        orbitOrigin = gridCenter;
+        // var gridCenter = XWFCAnimator.Instance.GetGridCenter();
+        // if (orbitOrigin.Equals(gridCenter)) return;
+        // orbitOrigin = gridCenter;
         Camera.transform.position = orbitOrigin;
         TranslateCameraViewDistance();
     }
 
     private void MouseInput()
     {
-        if (Input.GetMouseButton(0)) return; // Left.
+        if (Input.GetMouseButton(0)) PanCamera(); // Left.
         if (Input.GetMouseButton(1) && CameraMayMove()) OrbitCamera(CalcDeltaPos()); // Right.
         else if (Input.mouseScrollDelta.y != 0 && CameraMayMove()) ZoomCamera(Input.mouseScrollDelta.y);
+    }
+
+    private void PanCamera()
+    {
+        var delta = new Vector3(-Input.GetAxis("Mouse X"), -Input.GetAxis("Mouse Y"), 0);
+        var t = Camera.transform;
+        orbitOrigin += delta.x * t.right + delta.y * t.up;
+        Camera.transform.position = orbitOrigin;
+
     }
 
     private void OrbitCamera(Vector2 angle)
@@ -82,6 +98,12 @@ public class CameraMovementOrbit : MonoBehaviour
         pos += delta * t.forward;
         t.position = pos;
         _distanceFromOrigin = (pos - orbitOrigin).magnitude;
+    }
+
+    private void PanCameraX()
+    {
+        var t = Camera.transform;
+        orbitOrigin += 0.05f * t.right;
     }
 
     private Vector3 CalcDeltaPos()
